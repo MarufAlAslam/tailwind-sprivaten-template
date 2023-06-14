89e8574d3de10cdb4aa627cd905327de63206eda
marufAlAslam <maruf14@cse.pstu.ac.bd>
2023-06-14 17:35:43 +0600

    modal closer added

--- js/app.js
+++ js/app.js
@@ -1,30 +1,31 @@
-const menuBtn = document.getElementById('menu-btn');
-const menu = document.getElementById('menu');
+const menuBtn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
 
-const modal = document.getElementById('modal');
-const forms = document.querySelectorAll('form');
+const modal = document.getElementById("modal");
+const forms = document.querySelectorAll("form");
 
-const modalClose = document.querySelectorAll('.modal-close');
+const modalClose = document.getElementById("modal-close");
 
-menuBtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
-})
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
 
 const formSubmit = (e) => {
-    e.preventDefault();
-    modal.classList.remove('opacity-0');
-}
+  e.preventDefault();
+  modal.classList.remove("opacity-0");
+  modal.classList.remove("pointer-events-none");
+};
 
-forms.forEach(form => {
-    form.addEventListener('submit', formSubmit);
-})
+forms.forEach((form) => {
+  form.addEventListener("submit", formSubmit);
+});
 
-modal.addEventListener('click', () => {
-    modal.classList.add('opacity-0');
-})
+modal.addEventListener("click", () => {
+  modal.classList.add("opacity-0");
+  modal.classList.add("pointer-events-none");
+});
 
-modalClose.forEach(close => {
-    close.addEventListener('click', () => {
-        modal.classList.add('opacity-0');
-    })
-})
+modalClose.addEventListener("click", () => {
+  modal.classList.add("opacity-0");
+  modal.classList.add("pointer-events-none");
+});
